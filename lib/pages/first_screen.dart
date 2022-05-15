d40fcf315832d8a56e8c697a02565dfdaf7097d1
RafiaChy <rafiachowdhury24@gmail.com>
2022-05-15 20:31:19 +0600

    fixed list

--- lib/pages/first_screen.dart
+++ lib/pages/first_screen.dart
@@ -39,19 +39,7 @@ class _FirstScreenState extends State<FirstScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
              
-              children: choices.keys.map((word) => DragTarget<String>(
-              builder: (BuildContext context, List<String?> accepted, List<dynamic> rejected)
-             {
-               return drags("");
-             },
-             onWillAccept: (data) => data == word,
-             onAccept: (data){
-                setState(() {
-                  
-                });
-             },
-             
-              )).toList()..shuffle(Random(5))
+              children: choices.keys.map((word) => buildTarget(word)).toList()
             ),
           
           ],
@@ -68,6 +56,39 @@ class _FirstScreenState extends State<FirstScreen> {
                   height: 70,
                   child: Center(child: Text(e),),
                 );
+  }
+
+  Widget buildTarget(String word){
+
+    return DragTarget<String>(
+              builder: (BuildContext context, List<String?> accepted, List<dynamic> rejected)
+              
+             {
+                   if(word == choices.keys){
+                    return drags(word);
+                }
+               else{
+                 return Container(
+                   width: 50,
+                   height: 50,
+                   color: Colors.blueGrey,
+                 );
+               }
+               
+               },
+   
+             onWillAccept: (data) => data == word,
+             onAccept: (data){
+                 print('####'+data);
+                setState(() {
+                  drags(data);
+                });
+             },
+             
+              );
+    
+ 
+    
   }
  
 }
